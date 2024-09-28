bc6c8403d15e979d924bb25343c86135c3c84a5d
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-09-28 16:30:02 +0700

    first commit

--- src/app/components/navigasi/TimeZone.tsx
+++ src/app/components/navigasi/TimeZone.tsx
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from "react";
 const TimeZone = () => {
   const [time, setTime] = useState("");
   const [formattedDate, setFormattedDate] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
 
   useEffect(() => {
     const updateTime = () => {
@@ -41,47 +42,76 @@ const TimeZone = () => {
       ];
 
       const now = new Date();
-
       const dayName = dayNames[now.getDay()];
-
       const monthName = monthNames[now.getMonth()];
-
       let timeString = now.toLocaleTimeString("id-ID", options);
 
       timeString = timeString.replace(/\./g, ":");
-
       const [timePart, amPm] = timeString.split(" ");
 
       const dateString = `${dayName}, ${now.getDate()} ${monthName} ${now.getFullYear()}`;
-
       setFormattedDate(`ID 'UTC+7' - ${dateString}`);
       setTime(`'${timePart}'${amPm}`);
     };
 
     updateTime();
-    const intervalId = setInterval(updateTime, 1000);
+    const timeIntervalId = setInterval(updateTime, 1000);
+    const messageIntervalId = setInterval(() => {
+      setShowMessage((prev) => !prev);
+    }, 4000);
 
-    return () => clearInterval(intervalId);
+    return () => {
+      clearInterval(timeIntervalId);
+      clearInterval(messageIntervalId);
+    };
   }, []);
 
+  const handleNextClick = () => {
+    setShowMessage(true);
+  };
+
+  const handlePrevClick = () => {
+    setShowMessage(false);
+  };
+
   return (
-    <div className="w-full bg-black flex items-center justify-center p-2 gap-[6px] md:gap-10">
-      <div className="text-white">
-        <IconChevronLeft className="w-4 h-4" />
-      </div>
+    <div className="w-full bg-black">
+      <div className="max-w-sm lg:max-w-xl mx-auto flex items-center justify-between p-2">
+        <button className="text-white" onClick={handlePrevClick}>
+          <IconChevronLeft className="w-4 h-4" />
+        </button>
 
-      <div className="text-white flex gap-2">
-        <div className="text-center text-xs md:text-sm">
-          <p className="font-mono">{formattedDate}</p>
+        <div className="relative w-full mx-auto flex items-center justify-center">
+          <div
+            className={`relative text-center text-xs md:text-sm transition-opacity duration-700 ease-in-out ${
+              showMessage ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <p className="font-mono text-white">
+              THANK YOU FOR VISITING OUR WEBSITE
+            </p>
+          </div>
+
+          <div
+            className={`absolute transition-opacity duration-700 ease-in-out ${
+              showMessage ? "opacity-0" : "opacity-100"
+            }`}
+          >
+            <div className="text-white flex gap-[2px]">
+              <div className="text-center text-xs md:text-sm">
+                <p className="font-mono">{formattedDate}</p>
+              </div>
+
+              <div className="text-center text-xs md:text-sm">
+                <p className="font-mono">{time}</p>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div className="text-center text-xs md:text-sm">
-          <p className="font-mono">{time}</p>
-        </div>
-      </div>
-
-      <div className="text-white">
-        <IconChevronRight className="w-4 h-4" />
+        <button className="text-white" onClick={handleNextClick}>
+          <IconChevronRight className="w-4 h-4" />
+        </button>
       </div>
     </div>
   );
